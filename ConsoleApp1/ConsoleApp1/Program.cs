c64c6aa5b92985d53265b4a6ba2b8610bd5c2f84
yapeng <2672546346@qq.com>
2021-03-15 07:21:56 +0800

    add 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world 1");
+            Console.WriteLine("hello world 3");
             Console.ReadLine();
         }
     }
